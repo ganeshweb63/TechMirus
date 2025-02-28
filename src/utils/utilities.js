89fba605d86951847f5e912b2a78f62e98f5fd49
Ganesh Asimshetti <ganeshweb63@gmail.com>
2025-02-28 23:12:10 +0530

    Implement error response utilities.

---
+++ src/utils/utilities.js
@@ -0,0 +1,35 @@
+const { printLog, LogColor } = require("./logger");
+const StatusCode = require("./statusCodes");
+
+/**
+ *
+ * @param {*} error
+ * @returns Json String Object Error
+ */
+const buildErrorResponse = (error) => {
+  return JSON.stringify({
+    error: error,
+  });
+};
+
+/**
+ *
+ * @param {object} res Response Object
+ * @param {string} error Error Message
+ * @param {{statusCode:string}} options other options
+ */
+const sendErrorResponse = async (
+  res,
+  error,
+  options = { statusCode: StatusCode.badRequest }
+) => {
+  console.log(res);
+  const errorMessage = buildErrorResponse(error);
+  printLog(`[Route: ${res.req.originalUrl}] : ${errorMessage}`, {
+    color: LogColor.red,
+  });
+  res.status(options.statusCode);
+  await res.send(errorMessage);
+};
+
+module.exports = { buildErrorResponse, sendErrorResponse };
